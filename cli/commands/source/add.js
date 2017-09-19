76497edddfe5e84f357295cd229250539915b8af
Steven Meyer <steven@stdlib.com>
2017-09-19 09:54:06 -0700

    refactored source:add

--- cli/commands/source/add.js
+++ cli/commands/source/add.js
@@ -16,71 +16,69 @@ class SourceAdd extends Command {
   help() {
 
     return {
-      description: 'Convert a StdLib service into source code'
+      description: 'Converts a StdLib service into source code'
     };
 
   }
 
   run(params, callback) {
 
-    let pkg;
+    let pkgJSON;
 
     try {
-      pkg = require(path.join(process.cwd(), 'package.json'));
+      pkgJSON = require(path.join(process.cwd(), 'package.json'));
     } catch(e) {
       return callback(new Error('Invalid package.json'));
     }
 
-    if (!pkg.stdlib) {
+    if (!pkgJSON.stdlib) {
       return callback(new Error('No stdlib information set in "package.json"'));
     }
 
-    let source = require(path.join(__dirname,`../../templates/sourceCode/source.json`));
+    let sourceTemplate = require(path.join(__dirname,`../../templates/sourceCode/source.json`));
     let sourcePath = path.join(process.cwd(), 'source.json');
 
     if (fs.existsSync(sourcePath)) {
       return callback(new Error('source.json already exists'));
     }
+    
+    let envJSON;
 
-    let envPath = path.join(process.cwd(), 'env.json');
+    try {
+      envJSON = require(path.join(process.cdw(), 'env.json'));
+    } catch(e) {
+      return callback(new Error('Invalid env.json'));
+    }
+    
+    sourceTemplate.environmentVariables = {};
 
-    if (fs.existsSync(envPath)) {
-      //fill source.json with environment variables
-      source.environmentVariables = {};
-      let envJSON = require(envPath);
-
-
-      source.environmentVariables = {};
-
-      if (envJSON.release) {
-        for (var field in envJSON.release) {
-          source.environmentVariables[field] = {default: '', description: ''};
-        }
-      } else if (envJSON.dev) {
-        for (var field in envJSON.dev) {
-          source.environmentVariables[field] = {default: '', description: ''};
-        }
-      } else if (envJSON.local) {
-        for (var field in envJSON.local) {
-          source.environmentVariables[field] = {default: '', description: ''};
-        }
+    // prioritize release, then dev, then local  
+    if (envJSON.release) {
+      for (var field in envJSON.release) {
+        sourceTemplate.environmentVariables[field] = {default: '', description: ''};
       }
-
+    } else if (envJSON.dev) {
+      for (var field in envJSON.dev) {
+        sourceTemplate.environmentVariables[field] = {default: '', description: ''};
+      }
+    } else if (envJSON.local) {
+      for (var field in envJSON.local) {
+        sourceTemplate.environmentVariables[field] = {default: '', description: ''};
+      }
     }
 
     fs.writeFileSync(
       sourcePath,
-      JSON.stringify(source, null, 2)
+      JSON.stringify(sourceTemplate, null, 2)
     );
-
 
     let dirs = process.cwd().split(path.sep);
     let origin =  path.join(dirs[dirs.length - 2], dirs[dirs.length - 1])
-    pkg.stdlib.source = origin;
+    pkgJSON.stdlib.source = origin;
 
     fs.writeFileSync(
       path.join(process.cwd(), 'package.json'),
-      JSON.stringify(pkg, null, 2)
+      JSON.stringify(pkgJSON, null, 2)
     );
 
     console.log();
